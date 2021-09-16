69f2afce1ca034c500477db745791ff116ca39e9
Folkiwi <andersondiogo16@gmail.com>
2021-09-16 08:53:03 -0300

    dia 1

---
+++ Assets/Scripts/Ola.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Coé");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Oii sou o kiwi");
+    }
+}
